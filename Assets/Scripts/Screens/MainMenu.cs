fd27643fe53de246f17b16742df8cd9f256eaef4
cmehra96 <https://github.com/cmehra96>
2021-01-02 21:56:41 +0530

    Game Name Change

---
+++ Assets/Scripts/Screens/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Screens
+{
+    class MainMenu: MonoBehaviour
+    {
+        public static MainMenu Instance
+        {
+            get;
+            private set;
+        }
+
+        private void Awake()
+        {
+            Instance = this;
+
+        }
+
+        public void StartComputerPlay()
+        {
+            SceneManager.LoadScene("Game");
+        }
+
+    }
+}
